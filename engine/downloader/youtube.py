efb1d2ec95a0a6f318ad808c97d2876799efaba8
Harsh Prakash <harsh.prakash@apponward.com>
2026-05-25 15:05:05 +0530

    feat: change yt settings 25.05.2026

--- engine/downloader/youtube.py
+++ engine/downloader/youtube.py
@@ -8,18 +8,27 @@ class YouTubeDownloader(BaseDownloader):
     def get_ydl_opts(self):
         opts = super().get_ydl_opts()
 
+        # opts.update(
+        #     {
+        #         # Fallback format string to ensure it finds a valid format
+        #         "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",
+        #         # Merge into mp4 if possible
+        #         "merge_output_format": "mp4",
+        #         # Allow yt-dlp to execute YouTube JS extraction
+        #         "js_runtimes": {"nodejs": {}},
+        #         # Use mobile clients to bypass web JavaScript challenges and rate limits
+        #         "extractor_args": {
+        #             "youtube": ["player_client=android", "player_client=ios"]
+        #         },
+        #     }
+        # )
+
         opts.update(
             {
-                # Fallback format string to ensure it finds a valid format
-                "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",
-                # Merge into mp4 if possible
+                "format": "best",
                 "merge_output_format": "mp4",
-                # Allow yt-dlp to execute YouTube JS extraction
-                "js_runtimes": {"nodejs": {}},
-                # Use mobile clients to bypass web JavaScript challenges and rate limits
-                "extractor_args": {
-                    "youtube": ["player_client=android", "player_client=ios"]
-                },
+                "verbose": True,
+                "extractor_args": {"youtube": {"player_client": ["android"]}},
             }
         )
 
